73727a77a68fc5ca2a1dac728b28d3aba77e7ca2
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-02-08 22:41:09 +0530

    Create problem#70.java

---
+++ problem#70.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        if(n==0)
+        {
+            return 0;
+        }
+        if(n==1)
+        {
+            return 1;
+        }
+        int dp[]=new int[n];
+        dp[n-1]=1;
+        dp[n-2]=2;
+        for(int i=n-3;i>=0;i--)
+        {
+            dp[i]=dp[i+1]+dp[i+2];
+        }
+        
+        
+        return dp[0];
+        
+    }
+}
