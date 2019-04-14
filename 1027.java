e4424fd86624d29f3194531a5902406a625d52d0
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-04-14 09:27:40 +0530

    Create 1027.java

---
+++ 1027.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int longestArithSeqLength(int[] arr) {
+        
+        int n=arr.length;
+        if(n==1){
+            return 1;
+        }
+        int max=2;
+        int dp[][]=new int[n][n];
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int diff=arr[j]-arr[i];
+                int next=arr[j]+diff;
+                int count=2;
+                for(int k=j+1;k<n;k++){
+                    if(arr[k]==next){
+                        count++;
+                        next=arr[k]+diff;
+                    }
+                }
+                max=Math.max(max,count);
+            }
+        }
+        
+        return max;
+        
+    }
+}
