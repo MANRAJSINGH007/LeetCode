6bcc8772b6ddd9ed28cbe41dd27d1d3101236927
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-05-05 00:04:08 +0530

    Create 979.java

---
+++ 979.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    int ans;
+    public int distributeCoins(TreeNode root) {
+        ans = 0;
+        dfs(root);
+        return ans;
+    }
+
+    public int dfs(TreeNode node) {
+        if (node == null) return 0;
+        int L = dfs(node.left);
+        int R = dfs(node.right);
+        ans += Math.abs(L) + Math.abs(R);
+        return node.val + L + R - 1;
+    }
+}
