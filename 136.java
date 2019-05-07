0c8b59fb4df1ab7799ae88e4292081834f88505c
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-05-07 20:06:16 +0530

    Create 136.java

---
+++ 136.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int n=nums.length;
+        
+        int ans=0;
+        
+        for(int i=0;i<n;i++){
+            ans^=nums[i];
+        }
+        
+        return ans;
+        
+    }
+}
