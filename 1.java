0a0d815b242b6b8ac44f9003ccc7ac336d8aef28
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-05-28 23:03:35 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] twoSum(int[] a, int target) {
+        int b[]=new int[2];
+        int n=a.length;
+        for(int i=0;i<n-1;i++)
+        {   int flag=0;
+         
+            for(int j=i+1;j<n;j++)
+            {
+                if(a[i]+a[j]==target)
+                {
+                    b[0]=i;
+                    b[1]=j;
+                    flag=1;
+                    break;
+                }
+            }
+         
+         if(flag==1)
+         {
+             break;
+         }
+         
+        }
+        
+        return b;
+        
+    }
+}
