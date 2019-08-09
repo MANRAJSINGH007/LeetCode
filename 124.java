8e4654da41187fb8567dfa11154f32c188d890b8
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-08-09 22:16:02 +0530

    Create 124.java

---
+++ 124.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    static int max=Integer.MIN_VALUE;
+    
+    public static int func(TreeNode root){
+        
+        if(root==null){
+            return 0;
+        }
+        
+        int l=Math.max(func(root.left),0);
+        int r=Math.max(func(root.right),0);
+        
+        max=Math.max(l+r+root.val,max);
+        
+        return Math.max(l,r)+root.val;
+        
+    }
+    
+    public int maxPathSum(TreeNode root) {
+        
+        if(root==null){
+            return 0;
+        }
+        
+        max=Integer.MIN_VALUE;
+        int x=func(root);
+        return max;
+    }
+}
