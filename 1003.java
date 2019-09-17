88bbbbfec0fce9e413835d0770080d10daf23ac9
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-09-18 02:43:53 +0530

    Create 1003.java

---
+++ 1003.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isValid(String S) {
+        
+        int n = S.length();
+        
+        Stack<Character> stack = new Stack<>();
+        
+        int i = 0;
+        
+        while(i < n){
+            
+            stack.push(S.charAt(i));
+            
+            if(stack.size() >= 3){
+                
+                if(stack.peek() == 'c'){
+                    
+                    stack.pop();
+                    
+                    if(stack.peek() == 'b'){
+                        stack.pop();
+                        
+                        if(stack.peek() == 'a'){
+                            stack.pop();
+                        }
+                        
+                        else{
+                            stack.push('b');
+                            stack.push('c');
+                        }
+                    }
+                    
+                    else{
+                        stack.push('c');
+                    }
+                }
+            }
+            
+            i++;
+        }
+        
+        return stack.size() == 0;
+        
+    }
+}
