f14a4e07ff3eb57dab5edc84d23094e97acb4b6f
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-11-07 19:32:37 +0530

    Create 1108.java

---
+++ 1108.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String defangIPaddr(String address) {
+        
+        int n = address.length();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++){
+            char curr = address.charAt(i);
+            if(curr != '.') sb.append(curr);
+            else sb.append("[.]");
+        }
+        
+        return sb.toString();
+        
+    }
+}
