f6b04e3282b871ff060f45b098d74a20c6d9ba02
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-12-10 20:12:05 +0530

    Create 104.java

---
+++ 104.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        
+        if(root==null)
+        {
+            return 0;
+        }
+        int l=maxDepth(root.left)+1;
+        int r=maxDepth(root.right)+1;
+        if(l>r)
+        {
+            return l;
+        }
+        
+        return r;
+        
+    }
+}
