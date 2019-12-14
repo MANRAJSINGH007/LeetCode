893b32f38e822a5f299b3ec7e4cf4efb033a16b4
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-12-14 21:55:23 +0530

    Create biweekly-15-1.java

---
+++ biweekly-15-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        
+        int n = arr.length;
+        int count = 1;
+        int e = arr[0];
+        for(int i = 1; i < n; i++){
+            if(arr[i] == e) count++;
+            else {
+                e = arr[i];
+                count = 1;
+            }
+            
+            if(count > n/4) return arr[i];
+        }
+        
+        return arr[0];
+    }
+}
