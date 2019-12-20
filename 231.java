29ea1d95b8b3e7aec15024918c4c32402791bdf9
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-12-20 14:08:01 +0530

    Create 231.java

---
+++ 231.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        while(n > 0){
+            if(n == 1) return true;
+            if(n % 2 != 0) return false;
+            n /= 2;
+        }
+        return true;
+    }
+}
