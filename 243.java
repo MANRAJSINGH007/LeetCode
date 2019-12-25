349b548fc6b83f795e72db6c1d70936be8afeac9
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2019-12-25 10:35:13 +0530

    Update 243.java

--- 243.java
+++ 243.java
@@ -2,7 +2,7 @@ class Solution {
     public int shortestDistance(String[] words, String word1, String word2) {
         
         int n = words.length;
-        int l = 0, f = 0;
+        int l = -1, f = 0;
         int min = n - 1;
         for(int i = 0; i < n; i++){
             if(words[i].equals(word1)){
@@ -10,15 +10,15 @@ class Solution {
                 else{
                     if(f == 2) min = Math.min(min, i - l);
                     l = i;
-                    f = 1;
                 }
+                f = 1;
             } else if(words[i].equals(word2)){
                 if(l == -1) l = i;
                 else{
                     if(f == 1) min = Math.min(min, i - l);
                     l = i;
-                    f = 2;
                 }
+                f = 2;
             }
         }
         
