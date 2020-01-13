b96e6735fc12b8cf051a2cd6ee6b2b65a595460b
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2020-01-13 20:09:58 +0530

    Update 682.java

--- 682.java
+++ 682.java
@@ -6,16 +6,23 @@ class Solution {
         for(int i = 0; i < n; i++) {
             String curr = ops[i];
             int ascii = (int)curr.charAt(0);
-            if(curr.length() >= 2 || ascii >= 48 && ascii <= 57) stack.push(Integer.parseInt(curr));
+            if(curr.length() >= 2 || ascii >= 48 && ascii <= 57) {
+                int num = Integer.parseInt(curr);
+                stack.push(num);
+                ans += num;
+            }
             else if(curr.charAt(0) == '+') {
                 int temp1 = stack.pop();
                 int temp2 = stack.peek();
                 stack.push(temp1);
+                ans += temp1 + temp2;
                 stack.push(temp1 + temp2);
-            } else if(curr.charAt(0) == 'C') stack.pop();
-            else stack.push(2 * stack.peek());
+            } else if(curr.charAt(0) == 'C') ans -= stack.pop();
+            else {
+                ans += 2 * stack.peek();
+                stack.push(2 * stack.peek());
+            }
         }
-        while(stack.size() > 0) ans += stack.pop();
         return ans;
     }
 }
