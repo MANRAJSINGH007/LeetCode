e5f0f04a1d01739edf413f1fa078fe2aca31f315
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2020-03-22 10:33:01 +0530

    Create 1385.java

---
+++ 1385.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int count = 0;
+        outer : for(int i = 0; i < arr1.length; i++) {
+            int c = 0;
+            for(int j = 0; j < arr2.length; j++) {
+                if(Math.abs(arr1[i] - arr2[j]) <= d) continue outer;
+            }
+            count++;
+        }
+        return count;
+    }
+}
