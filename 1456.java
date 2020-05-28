16e1fd1e9b6c490d2cfc55314c5ef906dd601a22
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2020-05-28 20:01:22 +0530

    Create 1456.java

---
+++ 1456.java
@@ -0,0 +1,19 @@
+class Solution {
+    public static boolean check(char ch) {
+        return (ch == 'a' || ch == 'e' || ch == 'i' | ch == 'o' || ch == 'u');
+    }
+    public int maxVowels(String s, int k) {
+        int max = 0, n = s.length();
+        int count = 0;
+        for(int i = 0; i < k; i++) {
+            if(check(s.charAt(i))) count++;
+        }
+        max = count;
+        for(int  i = k; i < n; i++) {
+            if(check(s.charAt(i - k))) count--;
+            if(check(s.charAt(i))) count++;
+            max = Math.max(max, count);
+        }
+        return max;
+    }
+}
