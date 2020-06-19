3cb11ce7799682d7586dd933a6c923d6ddead237
MANRAJSINGH007 <35843207+MANRAJSINGH007@users.noreply.github.com>
2020-06-19 20:39:18 +0530

    Update 1356.java

--- 1356.java
+++ 1356.java
@@ -9,14 +9,17 @@ class Solution {
             public int compare(int[] x1, int[] x2) {
                 int e1 = x1[0], e2 = x2[0];
                 int c1 = 0, c2 = 0;
+                
                 while(e1 > 0) {
                     c1 += e1 % 2;
                     e1 /= 2;
-                } 
+                }
+                
                 while(e2 > 0) {
                     c2 += e2 % 2;
                     e2 /= 2;
                 }
+                
                 if(c1 < c2) return -1;
                 if(c2 < c1) return 1;
                 if(x1[0] < x2[0]) return -1;
